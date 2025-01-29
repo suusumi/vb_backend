4e5cd80592ceec00b9aa09fdc6a5d13e4d341197
Sergey Groshev <wer113wer@yandex.ru>
2025-01-29 18:56:05 +0300

    [fix](main): enable Cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+    const app = await NestFactory.create(AppModule);
+
+    const allowedOrigins = process.env.CORS_ORIGIN?.split(',');
+
+    app.enableCors({
+        origin: allowedOrigins,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    });
+
+    await app.listen(process.env.PORT ?? 3001);
 }
+
 bootstrap();
